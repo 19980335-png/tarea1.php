9c5c8b343c9b4eca5a7c88f6ab659e0ccf934730
19980335-png <19980335@clases.edu.sv>
2026-05-18 17:35:06 -0600

    ejercicio 18
    formulario que solicite el nombre del cliente, el producto, la cantidad y
    el precio unitario. En otro archivo PHP deberá mostrarse una factura con el
    subtotal y el total a pagar.

---
+++ resultado18.php
@@ -0,0 +1,15 @@
+<?php
+
+$cliente = $_POST['cliente'];
+$producto = $_POST['producto'];
+$cantidad = $_POST['cantidad'];
+$precio = $_POST['precio'];
+
+$subtotal = $cantidad * $precio;
+
+echo "Cliente: ".$cliente."<br>";
+echo "Producto: ".$producto."<br>";
+echo "Subtotal: ".$subtotal."<br>";
+echo "Total: ".$subtotal;
+
+?>
